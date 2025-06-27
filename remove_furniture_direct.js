5f110d646d1ca95663ea0b7d616f01c50080d4d4
danapixels <danaespine@gmail.com>
2025-06-27 05:51:16 -0400

    Furniture removal

---
+++ remove_furniture_direct.js
@@ -0,0 +1,32 @@
+// This script should be run inside the server container
+// Usage: docker exec -it iamafk-server-1 node remove_furniture_direct.js <furniture_id>
+
+const furnitureIdToRemove = process.argv[2];
+
+if (!furnitureIdToRemove) {
+console.error('Usage: node remove_furniture_direct.js <furniture_id>');
+console.error('Example: node remove_furniture_direct.js FCC_x5Q4Y6mfCnwbAAAn');
+process.exit(1);
+
+
+// Import the server's furniture object (this assumes the server is running)
+// We'll need to access the global furniture object from the server
+
+console.log(`Attempting to remove furniture: ${furnitureIdToRemove`);
+
+// Since we can't directly access the server's memory, let's use a different approach
+// We can send a signal to the server process to remove the furniture
+
+const fs = require('fs');
+const path = require('path');
+
+// Create a temporary file with the furniture ID to remove
+const tempFile = '/tmp/remove_furniture.txt';
+fs.writeFileSync(tempFile, furnitureIdToRemove);
+
+console.log(`Created removal request file: ${tempFile`);
+console.log('The server will need to be modified to check this file periodically');
+console.log('For now, you may need to restart the server or manually remove the furniture from the database');
+
+// Alternative: If you have access to the server's data file, we can remove it directly
+// This depends on how your server stores furniture data 
--- remove_specific_furniture.js
+++ remove_specific_furniture.js
@@ -9,8 +9,12 @@ console.error('Example: node remove_specific_furniture.js FCC_x5Q4Y6mfCnwbAAAn')
 process.exit(1);
 
 
-// Connect to the server
-const socket = io('http://localhost:3001');
+// Connect to the server through Docker network
+// The server container name is 'iamafk-server-1'
+const socket = io('http://iamafk-server-1:3001', {
+timeout: 5000,
+forceNew: true
+);
 
 socket.on('connect', () => {
 console.log('Connected to server');
@@ -25,6 +29,34 @@ console.log('Disconnecting...');
 socket.disconnect();
 process.exit(0);
 , 2000);
+);
+
+socket.on('connect_error', (error) => {
+console.error('Connection error:', error.message);
+console.log('Trying alternative connection...');
+
+// Try connecting to localhost as fallback
+const fallbackSocket = io('http://localhost:3001', {
+timeout: 5000,
+forceNew: true
+);
+
+fallbackSocket.on('connect', () => {
+console.log('Connected via fallback');
+fallbackSocket.emit('deleteFurniture', furnitureIdToRemove);
+console.log(`Attempting to delete furniture: ${furnitureIdToRemove`);
+
+setTimeout(() => {
+console.log('Disconnecting...');
+fallbackSocket.disconnect();
+process.exit(0);
+, 2000);
+);
+
+fallbackSocket.on('connect_error', (fallbackError) => {
+console.error('Fallback connection also failed:', fallbackError.message);
+process.exit(1);
+);
 );
 
 socket.on('disconnect', () => {
